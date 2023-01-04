6c267fcfab56d64d69a48b6e923e7269316099d7
星梦StarsDream <starsdream00@icloud.com>
2023-01-05 00:54:06 +0800

    Init

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -3,20 +3,18 @@
  * @brief The main file of the plugin
  */
 
-#include <llapi/LoggerAPI.h>
-
-#include "version.hpp"
-
-// We recommend using the global logger.
-extern Logger logger;
-
 /**
  * @brief The entrypoint of the plugin. DO NOT remove or rename this function.
- *        
+ *
  */
-void PluginInit()
-{
-    // Your code here
-    Logger logger(PLUGIN_NAME);
-    logger.info("Hello, world!");
+#include <llapi/HookAPI.h>
+#include <llapi/mc/Actor.hpp>
+#include <llapi/mc/ItemActor.hpp>
+#include <llapi/mc/ItemStack.hpp>
+#include <llapi/mc/MovementInterpolatorComponent.hpp>
+
+TInstanceHook(class ItemActor const*, "?_drop@Actor@@IEAAPEBVItemActor@@AEBVItemStack@@_N@Z", Actor, class ItemStack const& a2, bool a3) {
+	auto get = SymCall("??$tryGetComponent@UMovementInterpolatorComponent@@@EntityContextBase@@QEAAPEAUMovementInterpolatorComponent@@XZ", struct MovementInterpolatorComponent*, char*)((char*)this + 8);
+	*(int*)(get + 28) = 0;
+	return original(this, a2, a3);
 }
